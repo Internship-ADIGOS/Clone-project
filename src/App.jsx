285366bfcd9565863b0b6b0f388500b78e3d47d9
piyushjha0409 <peeyush0409@gmail.com>
2023-01-24 15:19:08 +0530

    setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Switch, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Route>
+      
+    </Route>
+  )
+}
+
+export default App
---
+++ src/Home/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import chart from '../components/Chart'
+
+const Homepage = () => {
+  return (
+      <>
+      <div>This is a Homepage</div>
+      <Navbar />
+      <chart />
+      </>
+
+  )
+}
+
+export default Homepage
